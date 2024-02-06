f6c75033596e4a88fdf44b5e4b3dc7f5825458c3
Roman Ryndych <r.ryndych@indev.expert>
2024-02-06 12:16:05 +0600

    Раскидал по отдельным файликам main функцию  с роутингом

--- main/main.go
+++ main/main.go
@@ -1,29 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"opencv/main/application"
 )
 
 func main(){
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
-	router.Get("/hello",basicHandler)
-
-	server:=&http.Server{
-		Addr:":3000",
-		Handler: router,
+	app:=application.New()
+	err:=app.Start(context.TODO())
+	if err != nil {
+		fmt.Println("failed to start app:",err)
 	}
-
-	err:=server.ListenAndServe()
-	if err!=nil{
-		fmt.Println("failed to start server",err)
-	}
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("Hello world"))
 }
